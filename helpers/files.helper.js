c9aeffe2f77fc971c2595bc966bd737c6dc11e2f
Kutman Smanov <smanovkutman0258@gmail.com>
2021-07-28 11:27:50 +0600

    tablet refactoring v4

--- helpers/files.helper.js
+++ helpers/files.helper.js
@@ -2,18 +2,12 @@ const fs = require('fs');
 
 exports.saveBase64Image = async function (base64Data, fileName){
     fs.writeFile(fileName, base64Data, 'base64', function(err) {
-        if (err) {
-            return false;
-        }
-        return true;
+        return !err;
     });
 }
 
 exports.deleteFile = async function (fileName){
     fs.unlink(fileName,function(err) {
-        if (err) {
-            return false;
-        }
-        return true;
+        return !err;
     });
 }
